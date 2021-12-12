c2381c4b6963d9ffe9eab7afa33dca4cf3ea2cbc
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-12-12 11:03:42 +0800

    initial spring context debug

---
+++ Spring-Framework-v5.3.13/spring-context-debug/src/main/java/com/kapcb/ccc/MainApplication.java
@@ -0,0 +1,17 @@
+package com.kapcb.ccc;
+
+/**
+ * <a>Title: MainApplication </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: MainApplication <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2021/12/11 23:19
+ * @since 1.0
+ */
+public class MainApplication {
+
+	public static void main(String[] args) {
+	}
+}
---
+++ Spring-Framework-v5.3.13/spring-context-debug/src/main/java/com/kapcb/ccc/components/Bean.java
@@ -0,0 +1,14 @@
+package com.kapcb.ccc.components;
+
+/**
+ * <a>Title: Bean </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: Bean <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2021/12/11 23:21
+ * @since 1.0
+ */
+public interface Bean {
+}
