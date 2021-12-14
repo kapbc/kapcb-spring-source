51d99273b7addcc5a9c6743db78ed48a1306b53d
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-12-14 21:42:16 +0800

    add comments for AbstractRefreshableApplicationContext's refreshBeanFactory() method

--- Spring-Framework-v5.3.13/spring-context/src/main/java/org/springframework/context/support/AbstractRefreshableApplicationContext.java
+++ Spring-Framework-v5.3.13/spring-context/src/main/java/org/springframework/context/support/AbstractRefreshableApplicationContext.java
@@ -119,14 +119,19 @@ public abstract class AbstractRefreshableApplicationContext extends AbstractAppl
 	 */
 	@Override
 	protected final void refreshBeanFactory() throws BeansException {
+		// 如果存在 BeanFactory 则销毁 BeanFactory
 		if (hasBeanFactory()) {
 			destroyBeans();
 			closeBeanFactory();
 		}
 		try {
+			// 创建 DefaultListableBeanFactory 对象
 			DefaultListableBeanFactory beanFactory = createBeanFactory();
+			// 为了序列化自指定 id, 可以从 id 反序列化获取到 BeanFactory 对象
 			beanFactory.setSerializationId(getId());
+			// 定制 BeanFactory, 设置相关属性, 包括是否允许覆盖同名称的不同定义的对象以及循环依赖
 			customizeBeanFactory(beanFactory);
+			// 初始化 DocumentReader, 并进行 XML 文件读取及解析, 默认命名空间的解析, 自定标签的解析
 			loadBeanDefinitions(beanFactory);
 			this.beanFactory = beanFactory;
 		}
