209e6e72c59c4c3cdb22aca20c518a2c8efc2a49
kapcb <eircccallroot@yeah.net>
2021-12-15 16:30:20 +0800

    Create DebugBean.java
    add model interface DebugBean

---
+++ Spring-Framework-v5.3.13/spring-context-debug/src/main/java/com/kapcb/ccc/model/DebugBean.java
@@ -0,0 +1,20 @@
+package com.kapcb.ccc.model;
+
+/**
+ * <a>Title: Bean </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: Bean <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2021/12/11 23:21
+ * @since 1.0
+ */
+public interface DebugBean {
+
+	/**
+	 * say method
+	 */
+	void say();
+
+}
