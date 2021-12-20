32dc9c7ffdd86743d93983114fdad2e39a31a35a
kapcb <eircccallroot@yeah.net>
2021-12-20 18:49:12 +0800

    Update AbstractAutowireCapableBeanFactory.java
    add comments for AbstractAutowireCapableBeanFactory's createBeanInstance method

--- Spring-Framework-v5.3.13/spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ Spring-Framework-v5.3.13/spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -1271,15 +1271,17 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
 		// 确认需要创建 Bean 实例的类可以实例化
 		Class<?> beanClass = resolveBeanClass(mbd, beanName);
 
-		// 确保 beanClass 不为空, 且访问权限是 public
+		//确保 beanClass 不为空, 且访问权限是 public
 		if (beanClass != null && !Modifier.isPublic(beanClass.getModifiers()) && !mbd.isNonPublicAccessAllowed()) {
 			throw new BeanCreationException(mbd.getResourceDescription(), beanName,
 					"Bean class isn't public, and non-public access not allowed: " + beanClass.getName());
 		}
 
+		// 如果存在就返回一个 callBack 回调函数, 在 obtainFromSupplier 方法中调用对应的方法并转换成 BeanWrapper 类型
 		// 确保当前 BeanDefinition 中是否包含实例供应器。此处相当一个回调方法, 利用回调方法来创建 Bean 实例对象
 		Supplier<?> instanceSupplier = mbd.getInstanceSupplier();
 		if (instanceSupplier != null) {
+			// 包装成 BeanWrapper 类型返回
 			return obtainFromSupplier(instanceSupplier, beanName);
 		}
 
@@ -1303,6 +1305,7 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
 				// 因为在判断过程中会进行比较, 所以 Spring 会将解析、确定好的构造函数缓存到 BeanDefinition 中的 resolvedConstructorOrFactoryMethod 字段中
 				// 在下次创建相同 Bean 对象时, 直接从 BeanDefinition 中的属性 resolvedConstructorOrFactoryMethod 缓存的值获取, 避免再次解析
 				if (mbd.resolvedConstructorOrFactoryMethod != null) {
+					// 设置为已经解析
 					resolved = true;
 					autowireNecessary = mbd.constructorArgumentsResolved;
 				}
@@ -1313,15 +1316,19 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
 		if (resolved) {
 			// 构造器有参数
 			if (autowireNecessary) {
+				// 使用构造函数自动注入
 				return autowireConstructor(beanName, mbd, null, null);
 			}
 			else {
-				// 使用默认构造函数构造
+				// 使用默认构造函数实例化给定的 Bean 实例
 				return instantiateBean(beanName, mbd);
 			}
 		}
 
 		// Candidate constructors for autowiring?
+		// 如果上面的步骤没有解析好对应的构造函数, 这里看看有没有指定构造函数
+		// 具体是 SmartInstantiationAwareBeanPostProcessor 接口, 这个接口继承了
+		// InstantiationAwareBeanPostProcessor, 通过调用 determineCandidateConstructors 方法来确认有没有指定的构造函数
 		// 从 Bean 后置处理器中为自动装配寻找构造方法, 有且仅有一个有参构造器或者有且仅有 @Autowired 注解构造
 		Constructor<?>[] ctors = determineConstructorsFromBeanPostProcessors(beanClass, beanName);
 
@@ -1332,6 +1339,7 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
 		// 4.有参与构造函数参数列表中的参数
 		if (ctors != null || mbd.getResolvedAutowireMode() == AUTOWIRE_CONSTRUCTOR ||
 				mbd.hasConstructorArgumentValues() || !ObjectUtils.isEmpty(args)) {
+			// 使用构造函数自动注入
 			return autowireConstructor(beanName, mbd, ctors, args);
 		}
 
