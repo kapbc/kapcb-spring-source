07c14f3b4795aa167557609cbc3200f6a52a7f51
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-12-27 22:45:43 +0800

    add comments for AbstractBeanFactory's doGetBean method's getSingleton(String beanName, ObjectFActory<?> objectFactory) method

--- Spring-Framework-v5.3.13/spring-beans/src/main/java/org/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
+++ Spring-Framework-v5.3.13/spring-beans/src/main/java/org/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
@@ -268,13 +268,13 @@ public class DefaultSingletonBeanRegistry extends SimpleAliasRegistry implements
 	public Object getSingleton(String beanName, ObjectFactory<?> singletonFactory) {
 		// beanName 属性校验
 		Assert.notNull(beanName, "Bean name must not be null");
-		// 同步一级缓存, 保证线程安全
+		// 使用单例对象的高速缓存 Map 作为锁, 保证线程安全
 		synchronized (this.singletonObjects) {
 			// 从一级缓存中获取 beanName 对应的单例对象
 			Object singletonObject = this.singletonObjects.get(beanName);
 			// 如果获取不到对象
 			if (singletonObject == null) {
-				// 如果当前正在 destroy 中
+				// 如果当前正在 destroySingleton 中
 				if (this.singletonsCurrentlyInDestruction) {
 					// 抛出不允许创建 Bean 异常 : 在工厂的单例销毁时不允许创建单例 Bean (请勿在 destroy 方法中向 BeanFactory 请求获取 Bean 实例)
 					throw new BeanCreationNotAllowedException(beanName,
@@ -286,15 +286,25 @@ public class DefaultSingletonBeanRegistry extends SimpleAliasRegistry implements
 					// 创建共享的单实例 Bean
 					logger.debug("Creating shared instance of singleton bean '" + beanName + "'");
 				}
+
 				// 创建单例之前的回调, 默认实现将单例注册为当前正在创建中
 				beforeSingletonCreation(beanName);
+
+				// 表示生成了新单例对象的标记, 默认为 false, 表示没有生成新的单例对象
 				boolean newSingleton = false;
+				// 有抑制异常记录标记, 没有时为 true, 否则为 false
 				boolean recordSuppressedExceptions = (this.suppressedExceptions == null);
+
+				// 如果没有抑制异常记录
 				if (recordSuppressedExceptions) {
+					// 对抑制的异常列表进行实例化
 					this.suppressedExceptions = new LinkedHashSet<>();
 				}
 				try {
+					// 从单例工厂中获取对象, 调用在 AbstractBeanFactory 中传递进来的 ObjectFactory<T> 函数式接口
 					singletonObject = singletonFactory.getObject();
+
+					// 生成了新的单例对象标记为 true, 表示生成了新的单例对象
 					newSingleton = true;
 				}
 				catch (IllegalStateException ex) {
