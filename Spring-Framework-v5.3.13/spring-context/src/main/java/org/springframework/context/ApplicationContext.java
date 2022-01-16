b776dc971c9756be88518e3a1c68cec060c36470
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-01-16 21:06:48 +0800

    Add comments for ApplicationContext

--- Spring-Framework-v5.3.13/spring-context/src/main/java/org/springframework/context/ApplicationContext.java
+++ Spring-Framework-v5.3.13/spring-context/src/main/java/org/springframework/context/ApplicationContext.java
@@ -55,7 +55,6 @@ import org.springframework.lang.Nullable;
  * @see org.springframework.beans.factory.BeanFactory
  * @see org.springframework.core.io.ResourceLoader
  */
-
 public interface ApplicationContext extends EnvironmentCapable, ListableBeanFactory, HierarchicalBeanFactory,
 		MessageSource, ApplicationEventPublisher, ResourcePatternResolver {
 
