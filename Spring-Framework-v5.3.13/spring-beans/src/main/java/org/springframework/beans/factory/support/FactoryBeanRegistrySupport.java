3332d1abe4071271c510fca2922ce76b4fedbe86
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-01-21 20:17:52 +0800

    add comments for FactoryBeanRegistrySupport

--- Spring-Framework-v5.3.13/spring-beans/src/main/java/org/springframework/beans/factory/support/FactoryBeanRegistrySupport.java
+++ Spring-Framework-v5.3.13/spring-beans/src/main/java/org/springframework/beans/factory/support/FactoryBeanRegistrySupport.java
@@ -163,6 +163,7 @@ public abstract class FactoryBeanRegistrySupport extends DefaultSingletonBeanReg
 	 * @throws BeanCreationException if FactoryBean object creation failed
 	 * @see org.springframework.beans.factory.FactoryBean#getObject()
 	 */
+
 	private Object doGetObjectFromFactoryBean(FactoryBean<?> factory, String beanName) throws BeanCreationException {
 		Object object;
 		try {
