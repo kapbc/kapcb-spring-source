80982bccb179d646ab7104fa5b3f6a514903815c
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-01-30 00:47:43 +0800

    add comments for AbstractApplicationContext

--- Spring-Framework-v5.3.13/spring-context/src/main/java/org/springframework/context/support/AbstractApplicationContext.java
+++ Spring-Framework-v5.3.13/spring-context/src/main/java/org/springframework/context/support/AbstractApplicationContext.java
@@ -1183,7 +1183,6 @@ public abstract class AbstractApplicationContext extends DefaultResourceLoader
 
 
 
-
 		if (this.shutdownHook == null) {
 			// No shutdown hook registered yet.
 			this.shutdownHook = new Thread(SHUTDOWN_HOOK_THREAD_NAME) {
