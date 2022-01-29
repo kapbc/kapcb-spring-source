283cf669256f1497b71b606671c456f83d163f6a
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-01-29 12:58:33 +0800

    add comments for AbstractApplicationContext

--- Spring-Framework-v5.3.13/spring-context/src/main/java/org/springframework/context/support/AbstractApplicationContext.java
+++ Spring-Framework-v5.3.13/spring-context/src/main/java/org/springframework/context/support/AbstractApplicationContext.java
@@ -1165,7 +1165,6 @@ public abstract class AbstractApplicationContext extends DefaultResourceLoader
 
 
 
-
 		AnnotationUtils.clearCache();
 		ResolvableType.clearCache();
 		CachedIntrospectionResults.clearClassLoader(getClassLoader());
